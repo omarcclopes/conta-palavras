401b622646f8177087ca8773df3fbaae3ff3d77a
omarcclopes <69813033+omarcclopes@users.noreply.github.com>
2020-10-04 16:28:12 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # conta-palavras
- 
+
+ # 20.2 P2 Linguagem de Programação
+
+# Construa um programa em que o usuário passe o nome do arquivo e receba como resposta número inteiro referente a quantidade de palavras dentro do arquivo;
+# - Nesta questão utilize os arquivos em anexo para testar;
+# - Coloque o projeto no Github e passe apenas o link nesta questão;
+# - Utilize todo os conceitos aprendidos de abertura de arquivo e tratamento de exceções.
--- conta_palavras.py
+++ conta_palavras.py
@@ -1,3 +1,9 @@
+# Atividade 20.2 P2 Linguagem de Programação - Omar Cunha Lopes
+# Professor Messias R. Batista
+# Construa um programa em que o usuário passe o nome do arquivo 
+# e receba como resposta número inteiro referente a quantidade 
+# de palavras dentro do arquivo.
+# 
 def wordcount(filename):
     try:
         with open(filename) as file:
@@ -10,7 +16,7 @@ def wordcount(filename):
         words = reader.split()
         num_words = len(words)
 
-        print("O numero de palavras no arquivo %s é aproximadamente %d!" %(filename, num_words))
+        print("O numero de palavras no arquivo %s é de %d aproximadamente!" %(filename, num_words))
 
 # Conta as palavras dos três livros txt
 wordcount('book_alice.txt')
